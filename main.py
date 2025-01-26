c9f1434de013cadaf1f3de598ba0599585271db7
JuanVeranoMesa <juanho2007@gmail.com>
2025-01-25 20:56:11 -0500

    Finalized a word and letter counter for one specific text!

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ with open("books/frankenstein.txt") as f:
     
 def word_count():
     total_words = len(file_contents.split())
-    print(total_words)
+    
+    return total_words
     
 def character_count():
     lower_case_book = file_contents.lower()
@@ -11,19 +12,21 @@ def character_count():
     char_count = {}
     
     for char in characters:
-        if char in char_count:
-            char_count[char] += 1
-        else:
-            char_count[char] = 1
+        if char.isalpha():
+            if char in char_count:
+                char_count[char] += 1
+            else:
+                char_count[char] = 1
     
     for char, count in char_count.items():
-        print(f"'{char}': {count}")
+        print(f"The letter '{char}' was found in this text {count} time(s)")
 
 def main():
     
     #print(file_contents) saved bc its annoying to see printed each time
+    print("Welcome to this report on frankenstein.txt.\n")
     
-    # word_count() temporarily not running this
+    print(f"There are a total of {word_count()} words in this book.\n")
     
     character_count()
     
