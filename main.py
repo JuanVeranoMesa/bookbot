4d8074541b9e8ca3f83dbfb0099b3b4e15c434f7
JuanVeranoMesa <juanho2007@gmail.com>
2025-01-25 20:00:08 -0500

    Wrote a script to read and print out frank.

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("hello world")
+with open("books/frankenstein.txt") as f:
+    file_contents = f.read()
+    
+def main():
+    print(file_contents)
+    
+if __name__ == "__main__":
+    main()
+    
